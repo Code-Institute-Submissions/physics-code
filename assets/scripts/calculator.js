f5d7f55ab2c8266993e7e8c6318c9c2125757ce3
pauld0051 <pauld0051@gmail.com>
2020-08-20 14:15:43 +0000

    Added units to calculations and dilemma zone number

--- assets/scripts/calculator.js
+++ assets/scripts/calculator.js
@@ -6,24 +6,24 @@ function dilemmaZone()
         ip = parseFloat(document.getElementById("interPhase").value);
         length = parseFloat(document.getElementById("length").value);
         let phase = yp + ip;
-document.getElementById("phaseDistance").innerHTML = (vi * phase) - length;
+document.getElementById("phaseDistance").innerHTML = (vi * phase) - length + " m";
         //Stopping distance - constant braking
         rt = parseFloat(document.getElementById("reactionTime").value);
         //unbraked distance travelled with reaction time - reaction distance (rd)
         let rd = rt * vi;
-document.getElementById("reactionDistance").innerHTML = (rd);
+document.getElementById("reactionDistance").innerHTML = (rd) + " m";
         //total stopping distance including reaction distance
         mu = parseFloat(document.getElementById("friction").value);
         //initial velocity squared for calculation
         let vsqr = Math.pow(vi,2);
         let sd = ((vsqr)/(2 * mu  * 9.81) + rd);
-document.getElementById("stoppingDistance").innerHTML = ((vsqr)/(2 * mu  * 9.81) + rd).toFixed(2);
+document.getElementById("stoppingDistance").innerHTML = ((vsqr)/(2 * mu  * 9.81) + rd).toFixed(2) + " m";
         id = parseFloat(document.getElementById("intersectionDistance").value);
         //maxDistance (md) is the distance from the stop-line a vehicle can safely travel through the intersection
         let md = (vi * phase) - length - id;
         document.getElementById("maxDistance").innerHTML = (md);
         //determine if a delimma zone exists (or option zone)
         let total = md - sd 
-document.getElementById("total").innerHTML = total.toFixed(2);            
+document.getElementById("total").innerHTML = total.toFixed(2) + " m";            
 
 }
