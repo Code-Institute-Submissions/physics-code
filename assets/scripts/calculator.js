3b4d410a5ccbe0ee6004dea8f76fd771e22a6b1f
pauld0051 <pauld0051@gmail.com>
2020-08-21 12:39:20 +0000

    Fixed decimal places for outputs

--- assets/scripts/calculator.js
+++ assets/scripts/calculator.js
@@ -1,7 +1,7 @@
 let finalVelocity;
 function convert() 
 {
-    vi = parseFloat(document.getElementById("initialVelocity").value);
+    let vi = parseFloat(document.getElementById("initialVelocity").value);
     var unit = document.getElementById("units").value;
     var result;
     if (unit == "ms") {
@@ -22,7 +22,7 @@ function convert()
 let fullDistance;
 function convertDistance()
 {
-    id = parseFloat(document.getElementById("intersectionDistance").value);
+    let id = parseFloat(document.getElementById("intersectionDistance").value);
     var lengthid = document.getElementById("length-units").value;
     var result;
     if (lengthid == "m") {
@@ -32,8 +32,8 @@ function convertDistance()
         result = id * 0.9144;
     }
     // passing the result outside the function, into the global variable for access later on
-    id = result;
-    document.getElementById("conversion-dist").innerHTML = id.toFixed(2);
+    fullDistance = result;
+    document.getElementById("conversion-dist").innerHTML = fullDistance.toFixed(2);
 }
 
 function dilemmaZone()
@@ -43,27 +43,31 @@ function dilemmaZone()
         let vf = finalVelocity;
         yp = parseFloat(document.getElementById("yellowPhase").value);
         ip = parseFloat(document.getElementById("interPhase").value);
-        length = parseFloat(document.getElementById("length").value);
+        vehicleLength = parseFloat(document.getElementById("length").value);
         let phase = yp + ip;
-document.getElementById("phaseDistance").innerHTML = (vf * phase) - length.toFixed(2) + " m";
+        //call phase distance
+document.getElementById("phaseDistance").innerHTML = ((vf * phase) - vehicleLength).toFixed(2) + " m";
         //Stopping distance - constant braking
         rt = parseFloat(document.getElementById("reactionTime").value);
         //unbraked distance travelled with reaction time - reaction distance (rd)
         let rd = rt * vf;
+        //call reaction distance
 document.getElementById("reactionDistance").innerHTML = (rd).toFixed(2) + " m";
         //total stopping distance including reaction distance
         mu = parseFloat(document.getElementById("friction").value);
         //initial velocity squared for calculation
         let vsqr = Math.pow(vf,2);
         let sd = ((vsqr)/(2 * mu  * 9.81) + rd);
+        //call stopping distance
 document.getElementById("stoppingDistance").innerHTML = ((vsqr)/(2 * mu  * 9.81) + rd).toFixed(2) + " m";
-        
+        let idt = fullDistance;
         //maxDistance (md) is the distance from the stop-line 
-        //a vehicle can safely travel through the intersection
-        let md = (vf * phase) - length - id;
-        document.getElementById("maxDistance").innerHTML = (md);
+        //any vehicle can safely travel through the intersection
+        //idt is the intersection distance total
+        let md = (vf * phase) - vehicleLength - idt;
+        document.getElementById("maxDistance").innerHTML = (md).toFixed(2);
         //determine if a delimma zone exists (or option zone)
-        let total = md - sd 
+        let total = md - sd; 
 document.getElementById("total").innerHTML = total.toFixed(2) + " m";            
 if (total > 0) {
     document.getElementById("zone").innerHTML = total.toFixed(2) + " m" + " option zone exists";
