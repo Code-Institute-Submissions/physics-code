33476577f892830400771c878aa31421c89f2c1a
pauld0051 <pauld0051@gmail.com>
2020-08-24 20:32:12 +0000

    Added game page with reaction timer to be scripted

---
+++ assets/scripts/game.js
@@ -0,0 +1,5 @@
+// Turn the traffic light green
+function makeGreen(){
+    document.getElementById("redLightStart").classList.add("icon_hide_redLight");
+    document.getElementById("greenLightStop").classList.remove("icon_hide_greenLight");
+}
