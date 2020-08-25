616cc095ee9e29528ea611ef40cf0a5c5fcc9b35
pauld0051 <pauld0051@gmail.com>
2020-08-25 15:49:14 +0000

    Added some functionality to timer before red light comes on

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,5 +1,24 @@
-// Turn the traffic light green
+// Turn the traffic light green when "start" button clicked and remove the start button
 function makeGreen(){
-    document.getElementById("redLightStart").classList.add("icon_hide_redLight");
-    document.getElementById("greenLightStop").classList.remove("icon_hide_greenLight");
+    $(redLightStart).addClass("icon_hide_redLight");
+    $(greenLightStop).removeClass("icon_hide_greenLight");
+    $(start_button).addClass("btn_hide_start");
 }
+
+// Set timer for how long light remains green
+
+function setTimerAmount(){
+    let timeInSeconds = Math.random() * (15 - 5) + 5;
+    setTimeout(function(){
+            $(brake_button).removeClass("btn_hide_brakes");
+            $(redLightStart).removeClass("icon_hide_redLight");
+            $(greenLightStop).addClass("icon_hide_greenLight");
+            //....and whatever else you need to do
+    }, timeInSeconds * 1000);
+}
+
+
+
+
+
+
