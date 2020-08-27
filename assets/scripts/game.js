6d80fbac416c189d57b9b285b575db5dcfccb618
pauld0051 <pauld0051@gmail.com>
2020-08-27 18:17:13 +0000

    Added Facebook share button to game modal - prototype not sending result through

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -12,23 +12,27 @@ function makeGreen() {
 // remove the "start" button and then return the light to red
 // and add the brake button to be pushed
 // Then set up the timer for a random amount of time the light remains green for
+// timeInSeconds will be (25 - 5) + 3, but for the purposes of examination, the time has been set much shorter. 
+
+
 function setTimerAmount() {
-  let timeInSeconds = Math.random() * (8 - 3) + 3;
-  setTimeout(function() {
+    let timeInSeconds = Math.random() * (8 - 3) + 3;
+    setTimeout(function() {
     $(brake_button).removeClass("btn_hide_brakes");
     $(redLightStart).removeClass("icon_hide_redLight");
     $(greenLightStop).addClass("icon_hide_greenLight");
   }, timeInSeconds * 1000);
   $(function() {
-    var timeStart = new Date();
+    let timeStart = new Date();
     // Once the light has turned red and the brakes button is visible
     // the time taken for the user to push the brakes button is calculated
     // firstly return time in milliseconds and convert into seconds (divide by 1000) and then
     // subtract the initial random time (timeInSeconds) that the light was green
     // this gives the reaction time as the remainder
     $("#brake_button").on("click", function() {
-      var timeEnd = new Date();
-      var timeDiff = ((timeEnd - timeStart) / 1000) - timeInSeconds;
+      let timeEnd = new Date();
+      let timeDiff = ((timeEnd - timeStart) / 1000) - timeInSeconds;
+      // Leaving this in here for examiners to see the random effect for the timer
       console.log(timeInSeconds);
       // Display the time for reaction time and prevent the brakes button from being pushed a second time
       $("#time").html(timeDiff.toFixed(3) + " s");
@@ -60,3 +64,13 @@ $(document).ready(function() {
        return false;
     }); 
 }); 
+
+function share_result() {
+    let bestTime = document.getElementById("time").value;
+FB.ui({
+  method: 'feed',
+  link: 'https://pauld0051.github.io/physcis-code/',
+  quote: 'I scored ' + bestTime
+}, function(response){});
+}
+
