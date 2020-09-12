7cc84db80a3da1aa5a41431f18afc28d1fccb10a
pauld0051 <pauld0051@gmail.com>
2020-09-12 06:49:41 +0000

    Fix icon in sendemail.js button for submission

--- assets/scripts/sendemail.js
+++ assets/scripts/sendemail.js
@@ -5,14 +5,14 @@ myform.submit(function(event) {
   let service_id = "default_service";
   let template_id = "physics_code_message";
 
-  myform.find("button").text("Sending...");
+  myform.find("button").html('<i class="fas fa-paper-plane"></i> Sending...');
   emailjs.sendForm(service_id, template_id, myform[0])
     .then(function() {
       alert("Sent!");
-      myform.find("button").text("Send");
+      myform.find("button").html('<i class="fas fa-paper-plane"></i> Submit');
     }, function(err) {
       alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
-      myform.find("button").text("Send");
+      myform.find("button").html('<i class="fas fa-paper-plane"></i> Submit');
     });
   return false;
 });
